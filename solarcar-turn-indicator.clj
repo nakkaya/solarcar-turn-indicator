0a16d4b46b0796a5912ab91cb3b3d821f128386c
Nurullah Akkaya <nurullah@nakkaya.com>
2016-08-03 21:32:09 +0300

    use a state machine

--- solarcar-turn-indicator.clj
+++ solarcar-turn-indicator.clj
@@ -1,19 +1,20 @@
 (configure-ferret! :command "mv solarcar-turn-indicator.cpp solarcar-turn-indicator.ino")
-(configure-runtime! FERRET_DISABLE_OUTPUT_STREAM true ;;save memory
-                    FERRET_MEMORY_POOL_SIZE 756) ;; Allocate 1kb Heap
+(configure-runtime! FERRET_DISABLE_OUTPUT_STREAM true
+                    FERRET_MEMORY_POOL_SIZE 756
+                    FERRET_MEMORY_POOL_PAGE_TYPE char
+                    FERRET_PROGRAM_MAIN program)
 
-(def left-signal-input 7)
-(def right-signal-input 8)
-(def left-signal-output 9)
-(def right-signal-output 10)
+(def left-input 7)
+(def right-input 8)
+(def left-output 9)
+(def right-output 10)
 (def debug-pin 13)
 
-(pin-mode left-signal-input :input)
-(pin-mode right-signal-input :input)
-(pin-mode left-signal-output :output)
-(pin-mode right-signal-output :output)
-(pin-mode debug-pin :output)
-(digital-write debug-pin :low)
+(pin-mode left-input   :input)
+(pin-mode right-input  :input)
+(pin-mode left-output  :output)
+(pin-mode right-output :output)
+(pin-mode debug-pin    :output)
 
 (defn flash-indicator [input output]
   (while (digital-read input)
@@ -25,9 +26,19 @@
     (sleep 500))
   (digital-write output :low))
 
-(forever
- (cond (digital-read left-signal-input)
-       (flash-indicator left-signal-input left-signal-output)
-
-       (digital-read right-signal-input)
-       (flash-indicator right-signal-input right-signal-output)))
+(def program
+  (state-machine 
+   (states
+    (signal (digital-write debug-pin :low)
+            (digital-write left-output :low)
+            (digital-write right-output :low))
+    
+    (turn-left  (flash-indicator left-input left-output))
+    (turn-right (flash-indicator right-input right-output)))
+   
+   (transitions
+    (signal
+     (fn [] (digital-read left-input)) turn-left
+     (fn [] (digital-read right-input)) turn-right)
+    (turn-left  (fn [] true) signal)
+    (turn-right (fn [] true) signal))))
